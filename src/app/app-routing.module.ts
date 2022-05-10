a0a3746d3efd39355ec0188337a37067968c865b
JimLeeWeiMin <JimLeeWeiMin@gmail.com>
2022-05-10 17:28:47 +0800

    angularroutingtest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'', redirectTo: 'pages/homepage', pathMatch: "full"
+  },
+  {
+    path:"pages/homepage",
+    component: HomepageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
