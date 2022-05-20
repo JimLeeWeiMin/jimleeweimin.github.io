dd2645e86291fdd461727637e2e4128568dae38e
JimLeeWeiMin <JimLeeWeiMin@gmail.com>
2022-05-21 00:15:12 +0800

    Setup Canvas in Scatter-Stuff

--- src/app/pages/scatter-stuff/scatter-stuff.component.ts
+++ src/app/pages/scatter-stuff/scatter-stuff.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-scatter-stuff',
   templateUrl: './scatter-stuff.component.html',
   styleUrls: ['./scatter-stuff.component.css']
 })
-export class ScatterStuffComponent implements OnInit {
+
+export class ScatterStuffComponent implements AfterViewInit {
+
+  // @ViewChild('myCanvas') canvasEl: ElementRef;
+  /** Template reference to the canvas element */
+  @ViewChild('canvasEl') 
+  private canvasEl: ElementRef = {} as ElementRef;
+
+  /** Canvas 2d context */
+  private ctx!: CanvasRenderingContext2D;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.ctx = this.canvasEl.nativeElement.getContext('2d');
   }
 
+  animate(): void {
+
+    this.ctx.fillStyle = 'rgb(200, 0, 0)';
+    this.ctx.fillRect(10, 10, 50, 50);
+
+    this.ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+    this.ctx.fillRect(30, 30, 50, 50);
+  }
 }
