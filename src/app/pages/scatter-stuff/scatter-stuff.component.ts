1040e5a9bf376410077bfbe39c2a0748225b6105
JimLeeWeiMin <JimLeeWeiMin@gmail.com>
2022-05-22 20:13:08 +0800

    Added ParticlesEmitter, Particles to ScatterStuff

--- src/app/pages/scatter-stuff/scatter-stuff.component.ts
+++ src/app/pages/scatter-stuff/scatter-stuff.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, ViewChild, ElementRef, AfterViewInit, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-scatter-stuff',
@@ -8,8 +9,7 @@ import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 export class ScatterStuffComponent implements AfterViewInit {
 
-  // @ViewChild('myCanvas') canvasEl: ElementRef;
-  /** Template reference to the canvas element */
+  //Targets the canvas that is inside the div
   @ViewChild('canvasEl') 
   private canvasEl: ElementRef = {} as ElementRef;
 
@@ -20,7 +20,18 @@ export class ScatterStuffComponent implements AfterViewInit {
   /** Canvas 2d context */
   private ctx!: CanvasRenderingContext2D;
 
-  constructor() { }
+  //Particle Emitter
+  private myPM!: ParticleEmitter;
+
+  //Stores the handle for animation frame
+  private animationFrameHandler!: number;
+
+  //Stores the lastTime to calculate deltaTime
+  private lastTime: number | null;
+
+  constructor(private _ngZone: NgZone) {
+    this.lastTime = null;
+  }
 
   ngAfterViewInit(): void {
     this.ctx = this.canvasEl.nativeElement.getContext('2d');
@@ -31,14 +42,150 @@ export class ScatterStuffComponent implements AfterViewInit {
     this.ctx.canvas.width = this.myIdentifer.nativeElement.offsetWidth;
     this.ctx.canvas.height = this.myIdentifer.nativeElement.offsetHeight;
 
-    
+    //Construct my particle emitter
+    this.myPM = new ParticleEmitter(this.ctx, 0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+  }
+
+  ngOnDestory() {
+    //Destory the animation frame
+    console.log("Destory the animation frame");
+    cancelAnimationFrame(this.animationFrameHandler);
   }
 
   animate(): void {
-    this.ctx.fillStyle = 'rgb(200, 0, 0)';
-    this.ctx.fillRect(10, 10, 50, 50);
+    //Start the animation frame
+    console.log("Animated");
+    this.animationFrameHandler = window.requestAnimationFrame(() => this.Update(this.myPM));
+  }
 
-    this.ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    this.ctx.fillRect(30, 30, 50, 50);
+  Update(_pm:ParticleEmitter):void {
+    //Request to call the function when the next animation frame occur
+    window.requestAnimationFrame(() => this.Update(_pm));
+
+    //Update the particle
+    _pm.Update()
+
+    //Draw the particle
+    _pm.Draw();
+  }
+}
+
+class Particle {
+  //Current position
+  posX: number = 0;
+  posY: number = 0;
+
+  //Current speed
+  velX: number = 0;
+  velY: number = 0;
+
+  //Boundary
+  minBoundX: number = 0;
+  minBoundY: number = 0;
+
+  maxBoundX: number = 0;
+  maxBoundY: number = 0;
+
+  //Calls when particle is constructed
+  constructor(_minBoundX: number, _minBoundY: number,
+              _maxBoundX: number, _maxBoundY: number,
+              _minVel: number, _maxVel: number) {
+                
+                this.posX = Math.floor(Math.random() * _maxBoundX) + _minBoundX;
+                this.posY = Math.floor(Math.random() * _maxBoundY) + _minBoundY;
+
+                this.velX = (Math.floor(Math.random() * _maxVel) + _minVel) * (Math.floor(Math.random() * 2) ? 1 : -1);
+                this.velY = (Math.floor(Math.random() * _maxVel) + _minVel) * (Math.floor(Math.random() * 2) ? 1 : -1);
+
+                this.minBoundX = _minBoundX;
+                this.minBoundY = _minBoundY;
+
+                this.maxBoundX = _maxBoundX;
+                this.maxBoundY = _maxBoundY;
+  }
+
+  //Updates the current particle location
+  Update() : void {
+
+    //Ensure that it is not out of bound
+    let tmpX = this.posX + this.velX;
+    let tmpY = this.posY + this.velY;
+
+    //If x is out of bound, recalculate the position
+    if(tmpX > this.maxBoundX) this.posX = this.minBoundX + (tmpX - this.maxBoundX);
+    else if(tmpX < this.minBoundX) this.posX = this.maxBoundX - (this.minBoundX - tmpX);
+    else this.posX = tmpX;
+
+    //If y is out of bound, recalculate the position
+    if(tmpY > this.maxBoundY) this.posY = this.minBoundY + (tmpY - this.maxBoundY);
+    else if(tmpY < this.minBoundY) this.posY = this.maxBoundY - (this.minBoundY - tmpY);
+    else this.posY = tmpY;
+  }
+}
+
+class ParticleEmitter {
+
+  //Maximum number of particle on the screen at any given time
+  maxParticle: number = 50;
+  //Current number of particle that is alive
+  currentParticle: number = 0;
+  //Stores all the particle
+  arrParticle: Particle[] = [];
+
+  //Stores the min and max position of spawn point
+  minBoundX: number = 0;
+  minBoundY: number = 0;
+
+  maxBoundX: number = 0;
+  maxBoundY: number = 0;
+
+  //Stores the min and max velocity of the particle
+  minVelocity: number = 1;
+  maxVelocity: number = 3;
+
+  //Stores the canvas
+  ctx!: CanvasRenderingContext2D;
+
+  constructor(_ctx: CanvasRenderingContext2D,_minBoundX: number, _minBoundY: number,
+              _maxBoundX: number, _maxBoundY: number) {
+                
+                //Set the context
+                this.ctx = _ctx;
+
+                //Set the boundary
+                this.minBoundX = _minBoundX;
+                this.minBoundY = _minBoundY;
+
+                this.maxBoundX = _maxBoundX;
+                this.maxBoundY = _maxBoundY;
+
+                //Construct the particle
+                for(let i = 0; i < this.maxParticle; ++i)
+                {
+                  this.arrParticle.push(new Particle( this.minBoundX, this.minBoundY,
+                                                      this.maxBoundX, this.maxBoundY,
+                                                      this.minVelocity, this.maxVelocity));
+                }
+  }
+
+  Update(): void {
+
+    //Update the particle
+    for(let i = 0; i < this.maxParticle; ++i)
+    {
+      this.arrParticle[i].Update();
+    }
+  }
+
+  Draw(): void {
+
+    //Clear the canvas
+    this.ctx.clearRect(this.minBoundX,this.minBoundY, this.maxBoundX, this.maxBoundY);
+
+    for(let i = 0; i < this.maxParticle; ++i)
+    {
+      this.ctx.fillStyle = 'rgb(200, 0, 0)';
+      this.ctx.fillRect(this.arrParticle[i].posX, this.arrParticle[i].posY, 10, 10);
+    }
   }
 }
