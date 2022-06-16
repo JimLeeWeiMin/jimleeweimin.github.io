a439fa5f46f3578d6828e9dcdfdf0be9c5ab3812
JimLeeWeiMin <JimLeeWeiMin@gmail.com>
2022-06-17 04:44:07 +0800

    Tablet support for navbar

--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
+  @ViewChild('TabletNavigationWrapper') 
+  private myTabletNavigationWrapper: ElementRef = {} as ElementRef;
+  private isClick: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  SideNavButtonOnClick(): void {
+    if(this.isClick)
+    {
+      this.isClick = false;
+      this.myTabletNavigationWrapper.nativeElement.style.visibility = "hidden";
+    }
+    else
+    {
+      this.isClick = true;
+      this.myTabletNavigationWrapper.nativeElement.style.visibility = "visible";
+      
+    }
   }
 
 }
